0976f478b09baf1df0e1477579711d74646f0f22
vladislav_volynets <flatislove@gmail.com>
2026-09-15 21:53:23 +0500

    web

--- loader.py
+++ loader.py
@@ -2,6 +2,7 @@ import os
 from aiogram import Bot, Dispatcher
 from aiogram.fsm.storage.memory import MemoryStorage
 from apscheduler.schedulers.asyncio import AsyncIOScheduler
+from aiohttp import web
 from dotenv import load_dotenv
 
 load_dotenv()
@@ -10,3 +11,4 @@ TOKEN = os.getenv("BOT_TOKEN")
 bot = Bot(token=TOKEN)
 dp = Dispatcher(storage=MemoryStorage())
 scheduler = AsyncIOScheduler()
+app = web.Application()
